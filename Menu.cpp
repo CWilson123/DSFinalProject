e0903f97bf515619da40e58c206ca6a4c79acdef
dallastealer <48176373+dallastealer@users.noreply.github.com>
2020-08-02 00:17:47 -0400

    New clean file with same functionality as before

--- Menu.cpp
+++ Menu.cpp
@@ -9,38 +9,5 @@ using namespace std;
 
 
 int main() {
-	string airline, origin, destination, header, test, index, day, hour, hasDelay, early, delay, departureTime;
-	vector<Flight*> flights;
-	int count = 0;
-
-	ifstream file("shortened.csv");
-	getline(file, header);
-	cout << "READING IN FILE" << endl;
-	cout << endl;
-	while (file.good()) {
-		count++;
-		if (count % 45831 == 0) {
-			cout << count << "/458311 Lines Read" << endl;
-		}
-		getline(file, index, ',');
-		getline(file, airline, ',');
-		getline(file, day, ',');
-		getline(file, origin, ',');
-		getline(file, destination, ',');
-		getline(file, departureTime, ',');
-		getline(file, delay, ',');
-		getline(file, hasDelay, ',');
-		getline(file, early, ',');
-		getline(file, hour, '\n');
-
-		Flight* toAdd = new Flight(airline, day, origin, destination, hour, delay, hasDelay, early);
-		flights.push_back(toAdd);
-
-		if (index == "469967") {
-			break;
-		}
-	}
-	cout << endl;
-	cout << "FILE LOADED" << endl;
-	
+	Flight::readFlights();
 }
